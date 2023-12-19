aee9d884c38eddc0c1de7f74dddd0d1a8ed9b14d
athulmani <athulathu2610@gmail.com>
2023-12-19 17:13:04 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Counter from './Counter';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Header/>
+     <Counter/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
